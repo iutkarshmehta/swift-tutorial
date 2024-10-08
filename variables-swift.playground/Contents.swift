28dc84a0aa37333fe524bfc2f1da6c6e6657b025
Utkarsh Mehta <utmehta@deloitte.com>
2024-10-08 23:50:30 +0530

    variable declaration complete

--- variables-swift.playground/Contents.swift
+++ variables-swift.playground/Contents.swift
@@ -1,5 +1,18 @@
 import UIKit
 
 //var is used to declare a variable which itself determine teh data type once a value is assigned to the variable
-var myName: String = "Utkarsh Mehta"
+var myName = "Utkarsh Mehta"
 print("My name is \(myName)")
+
+var index: Int = 13
+
+var inder1,counter, result: Double
+
+var x = 0.0,y = 0.1, z = 2.22
+
+//defining constant value
+
+let index2 = 20
+
+//index2 = 30
+// not allowed , throws error
