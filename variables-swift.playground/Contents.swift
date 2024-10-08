cff093826f2c1085cb8bb35bed55f9a897f0c5b7
Utkarsh Mehta <utmehta@deloitte.com>
2024-10-08 23:40:45 +0530

    variables

---
+++ variables-swift.playground/Contents.swift
@@ -0,0 +1,5 @@
+import UIKit
+
+//var is used to declare a variable which itself determine teh data type once a value is assigned to the variable
+var myName: String = "Utkarsh Mehta"
+print("My name is \(myName)")
