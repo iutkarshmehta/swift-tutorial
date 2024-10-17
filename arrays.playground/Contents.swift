17e224164262c399fda7edefa82a67692234e0fd
Utkarsh  Mehta <utkarshmehta4991@gmail.com>
2024-10-18 00:52:50 +0530

    arrays swift

---
+++ arrays.playground/Contents.swift
@@ -0,0 +1,27 @@
+import UIKit
+
+//Arrays
+
+var fastFood: [String] = ["Pizza", "Burger", "Fries"]
+
+//.first
+fastFood.first
+
+//.last
+fastFood.last
+
+// [0] index
+fastFood[0]
+
+// .count
+fastFood.count
+
+// .append()
+fastFood.append("Chicken")
+
+// .insert()
+fastFood.insert("duck", at: 2)
+
+// .remove()
+fastFood.remove(at: 2)
+
